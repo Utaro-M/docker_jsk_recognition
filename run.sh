8d41610866bed3a7d1a1e97f34efeb5e6a41abfe
matsuura <matsuura@jsk.imi.i.u-tokyo.ac.jp>
2021-10-09 14:19:32 +0900

    fix

--- run.sh
+++ run.sh
@@ -12,5 +12,5 @@ docker run --rm \
        --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
        --volume="$(pwd)/docker_bind:/mnt" \
        --volume="/home/fukazawa/.emacs.d:/root/.emacs.d/" \
-       -it utarom/jsk_recognition:buildall /bin/bash
+       -it utarom/jsk_recognition:buildall_publish_memory /bin/bash
 xhost +local:docker
